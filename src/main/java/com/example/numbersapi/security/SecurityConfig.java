4651faf570caca36e6dd218dc8cd8c8ad3bf0932
Zainab Alayande <alayandezainab64@gmail.com>
2025-02-02 13:54:53 +0100

    chore: updated security config

--- src/main/java/com/example/numbersapi/security/SecurityConfig.java
+++ src/main/java/com/example/numbersapi/security/SecurityConfig.java
@@ -4,20 +4,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securtyFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-        .cors(Customizer.withDefaults())
-        .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .authorizeHttpRequests(request ->
-                request.requestMatchers("/api/classify-number/**").permitAll()).build();
-    }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .cors(Customizer.withDefaults())
+                .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(request ->
+                        request.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
 
 }
